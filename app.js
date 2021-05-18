3ba359956ee6986f60485e8a793ff1e3bfe7e121
cody <cody.brown1993@gmail.com>
2021-05-18 16:24:33 -0400

    phase 2 complete

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
     res.send('Hello from Express!')
+})
+
+app.all('/:id', (req, res) => {
+    // console.log(req.params.id)
+    let random = Math.floor(Math.random() * 100)
+
+    res.render("index", { method: `${req.method}`, path: `${req.path}`, random_number: `${random}` })
+
 })
 
 const port = 8081
