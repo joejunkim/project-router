87294de53daa2b417d1fa7de144364d3d9121578
cody <cody.brown1993@gmail.com>
2021-05-18 16:54:02 -0400

    phase 4 complete

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ app.get(/(xyz)$/, (req, res) => {
     res.send("That's all I wrote.")
 })
 
+// app.get(/^(/capital-letters/)/, (req, res) => {
+//     res.send('test')
+//     // res.send.toUpperCase(req.params.id)
+// })
+app.get('/capital-letters/:id', (req, res) => {
+    // const id = req.params.id
+    // res.send(id.toUpperCase())
+    res.send(req.params.id.toUpperCase())
+})
+
 app.all('/:id', (req, res) => {
     // console.log(req.params.id)
     let random = Math.floor(Math.random() * 100)
