fa0fa936953e59538d432d6ace4cfdcf8d961523
Joe Kim <joekim6a@gmail.com>
2021-05-18 14:31:53 -0700

    Preparing Phase 5

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+const marRouter = require('.mar')
 
 app.set('view engine', 'pug')
 
@@ -8,22 +9,17 @@ app.get('/', (req, res) => {
     res.send('Hello from Express!')
 })
 
+app.use()
+
 app.get(/(xyz)$/, (req, res) => {
     res.send("That's all I wrote.")
 })
 
-// app.get(/^(/capital-letters/)/, (req, res) => {
-//     res.send('test')
-//     // res.send.toUpperCase(req.params.id)
-// })
 app.get('/capital-letters/:id', (req, res) => {
-    // const id = req.params.id
-    // res.send(id.toUpperCase())
     res.send(req.params.id.toUpperCase())
 })
 
 app.all('/:id', (req, res) => {
-    // console.log(req.params.id)
     let random = Math.floor(Math.random() * 100)
 
     res.render("index", { method: `${req.method}`, path: `${req.path}`, random_number: `${random}` })
